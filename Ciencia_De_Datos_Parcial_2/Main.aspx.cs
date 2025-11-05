3837442fca8782abf4065dd1ea985de1d07239a3
Lucas Emmanuel Saucedo <134143069+HerrRoniston@users.noreply.github.com>
2025-11-05 16:37:34 -0300

    Aporte 2
    Aporte 2: Calcular la conveniencia de invertir en la capital de cada banco considerando año completo, trimestres, meses.
    
    -He usado decimal en vez de float para mayor precisión.

--- Ciencia_De_Datos_Parcial_2/Main.aspx.cs
+++ Ciencia_De_Datos_Parcial_2/Main.aspx.cs
@@ -11,7 +11,28 @@ namespace Ciencia_De_Datos_Parcial_2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            //ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+
+
+        // Aporte 2 - Lucas
+        public decimal CalcularAnual(decimal capital, decimal tasaAnualPromedio)
+        {
+            return capital * (1 + tasaAnualPromedio);
+        }
+
+        // Función para la Modalidad 2
+        public decimal CalcularTrimestral(decimal capital, decimal tasaAnualPromedio)
+        {
+            decimal tasaTrimestral = tasaAnualPromedio / 4;
+            return capital * (decimal)Math.Pow(1 + (double)tasaTrimestral, 4);
+        }
+
+        // Función para la Modalidad 3
+        public decimal CalcularMensual(decimal capital, decimal tasaAnualPromedio)
+        {
+            decimal tasaMensual = tasaAnualPromedio / 12;
+            return capital * (decimal)Math.Pow(1 + (double)tasaMensual, 12);
         }
     }
 }
