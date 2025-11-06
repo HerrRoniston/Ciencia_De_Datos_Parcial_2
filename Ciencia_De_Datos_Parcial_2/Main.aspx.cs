58cd5a63c4b1a953d1aa55e0daee6310b35f59d7
GpabloQ <pablo.questa@alumnos.frgp.utn.edu.ar>
2025-11-06 01:07:24 -0300

    Aporte 1: Convierte los valores ingresados a float y se guardardan. Se calculan los promedios de las tasas anuales de cada banco

--- Ciencia_De_Datos_Parcial_2/Main.aspx.cs
+++ Ciencia_De_Datos_Parcial_2/Main.aspx.cs
@@ -13,7 +13,57 @@ namespace Ciencia_De_Datos_Parcial_2
         {
             //ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
         }
+        // APORTE 1 - Pablo
+        public void CalcularPromedios()
+        {
+            try
+            {
+                // 1️⃣ Convertir los valores ingresados a float y guardarlos en listas
+                List<float> tasasProvincia = new List<float>
+                {
+                    float.Parse(txtProv1.Text),
+                    float.Parse(txtProv2.Text),
+                    float.Parse(txtProv3.Text)
+                };
 
+                List<float> tasasNacion = new List<float>
+                {
+                    float.Parse(txtNac1.Text),
+                    float.Parse(txtNac2.Text),
+                    float.Parse(txtNac3.Text)
+                };
+
+                List<float> tasasHipotecario = new List<float>
+                {
+                    float.Parse(txtHip1.Text),
+                    float.Parse(txtHip2.Text),
+                    float.Parse(txtHip3.Text)
+                };
+
+                // 2️⃣ Calcular los promedios
+                float promedioProv = CalcularPromedio(tasasProvincia);
+                float promedioNac = CalcularPromedio(tasasNacion);
+                float promedioHip = CalcularPromedio(tasasHipotecario);
+
+                // 3️⃣ Mostrar los resultados en pantalla
+                lblMensaje.Text = "PROMEDIOS ANUALES:<br/>" +
+                                  $"BANCO PROVINCIA: {promedioProv:F2}%<br/>" +
+                                  $"BANCO NACION: {promedioNac:F2}%<br/>" +
+                                  $"BANCO HIPOTECARIO: {promedioHip:F2}%<br/><br/>";
+            }
+            catch (FormatException)
+            {
+                lblMensaje.Text = "ERROR: INGRESE SOLO NUMEROS VALIDOS EN TODAS LAS TASAS.";
+            }
+        }
+
+        private float CalcularPromedio(List<float> tasas)
+        {
+            float suma = 0;
+            foreach (float t in tasas)
+                suma += t;
+            return suma / tasas.Count;
+        }
 
         // Aporte 2 - Lucas
         public decimal CalcularAnual(decimal capital, decimal tasaAnualPromedio)
@@ -46,6 +96,7 @@ namespace Ciencia_De_Datos_Parcial_2
 
         protected void btnCalcularInversion_Click(object sender, EventArgs e)
         {
+            CalcularPromedios();
             /* EJEMPLO DE COMO SE USARIA (no tengo ni idea de como quieren hacer o divirlo asi que lo hice, cualquier cosa me avisan)
             decimal[] bp = { 1, 2, 3 };
             decimal[] bn = { 4, 5, 6 };
